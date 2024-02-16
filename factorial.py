459fb6d539f54c596d152c483b7f8acb323feaa1
Leo Boekestein <lb838@blue18.ex.ac.uk>
2024-02-16 13:46:58 +0000

    second commit

--- factorial.py
+++ factorial.py
@@ -8,10 +8,10 @@ def factorial(n):
 	return {"result": r}, 200
 
 def calculate(n):
-	if n <= 1:
+	if n <= 1: #Both 0 and 1 have the same factorial result so this is recursive base
 		return 1
 	else:
-		return n * calculate(n-1)
+		return n * calculate(n-1) #Recursive step
 
 if __name__ == "__main__":
 	app.run(host="localhost", port=6023)
